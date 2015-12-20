897845b52f7661d43c87f51883907912bc44805c
Chris Dawson <xrdawson@gmail.com>
2015-12-19 23:19:51 -0800

    Original checkin

---
+++ app.js
@@ -0,0 +1,48 @@
+
+// This script reports its own src at /s[rc]
+
+// This poem is beautiful and important to execution
+// 
+// -Happy Birthday OpenStack, with a #createServer
+// Content-type: writeHead and format.
+// vmc_app_port -> toUpperCase.
+// http or url ^ envious?
+// text/plain for $, but how much?
+// *100%disappearing :: format.
+// +end.  So sad.
+
+// Use magical l33t h@x0r number many times, top and bottom, FTW!
+_0x1337 = 1337
+sl = 'slice'
+l = function () { 
+l = ' listen'; 
+s = l[3]//3t
+return l
+}()[sl](_0x1337/1337)
+
+rf = 'readFileSync', fs = 'fs', re = require, fi = __filename
+sr = ''+re(fs)[rf](fi)
+se = 'search', su = 'substr', l2 = 'length', u = 'util'
+pi = parseInt, pr = process
+
+i = function(s,o,e){ return sr[su](sr[se](s)+o,e) }
+e2 = i(/\^ /,2,3), f = i(/::/,3,6)
+z = i(/\*/,2,2), fo = i(/\*/,4,2)
+$ = i('-',1,24), c = i('#',1,12)
+w = i(':',2,9), e = i(/\+/,1,3)//3t!
+c2 = i(": ",-14,12),h = i(/\^/, -12,4)
+_ = i(/\$/,-15,10),ur = i(/\^/, -4,3)
+
+n = function() {
+    x = i('->',-13,12)+""
+    up = i(x,x[l2]+4,11)//33t!
+    p = x[up]()+""
+}()
+
+re(h)[c](function(rq, rs) {
+    // l33t h@x0r number supercharges hTtp: code
+    rs[w](pi(pi(re(u)[f](fo,pi(($[l2]/_0x1337)+(133>>7)+(133>>7))+z))),{}[c2]=_)
+    rs[e](rq[ur]&&rq[ur][1]==s?sr:$)
+})[l](pr[e2][p]||_0x1337)
+
+// Handcoded (no minimizers) by xrd, webiphany.com
